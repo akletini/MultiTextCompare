277caef3cfcf6164c5bfb6d5bd09ed5bb96c1003
akletini <allen.kletinitch@smail.th-koeln.de>
2020-12-02 23:44:49 +0100

    Config Import implementiert

--- MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IFileImporterImpl.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IFileImporterImpl.java
@@ -1,7 +1,11 @@
 package de.thkoeln.syp.mtc.steuerung.impl;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 import de.thkoeln.syp.mtc.datenhaltung.impl.IConfigImpl;
 import de.thkoeln.syp.mtc.steuerung.services.IFileImporter;
@@ -16,6 +20,14 @@ public class IFileImporterImpl implements IFileImporter {
 		return iConfigImpl;
 	}
 
+	public boolean setConfig(IConfigImpl iConfigImpl) {
+		if (iConfigImpl != null) {
+			this.iConfigImpl = iConfigImpl;
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public List<File> getTextdateien() {
 		return textdateien;
@@ -23,8 +35,38 @@ public class IFileImporterImpl implements IFileImporter {
 
 	@Override
 	public boolean importConfigdatei() {
-		// TODO Auto-generated method stub
-		return false;
+		File configFile = new File("config\\config.properties");
+		try {
+
+			FileReader reader = new FileReader(configFile);
+
+			Properties props = new Properties();
+
+			props.load(reader);
+
+			String savedRootDir = props.getProperty("savedRootDir");
+			boolean beachteLeerzeichen = Boolean.parseBoolean(props
+					.getProperty("beachteLeerzeichen"));
+			boolean beachteGrossschreibung = Boolean.parseBoolean(props
+					.getProperty("beachteGrossschreibung"));
+			boolean beachteSatzzeichen = Boolean.parseBoolean(props
+					.getProperty("beachteSatzzeichen"));
+
+			reader.close();
+
+			iConfigImpl.setRootDir(savedRootDir);
+			iConfigImpl.setBeachteLeerzeichen(beachteLeerzeichen);
+			iConfigImpl.setBeachteGrossschreibung(beachteGrossschreibung);
+			iConfigImpl.setBeachteSatzzeichen(beachteSatzzeichen);
+
+		} catch (FileNotFoundException fnf) {
+			fnf.printStackTrace();
+			return false;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 	@Override
@@ -38,4 +80,5 @@ public class IFileImporterImpl implements IFileImporter {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 }
