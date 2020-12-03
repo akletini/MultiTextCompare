46e2b1eaf7160f17c05db7481cc606a6bd23a362
luckerma <luca_simon.uckermann@smail.th-koeln.de>
2020-12-03 11:04:24 +0100

    IFileImporter Config Verwaltung implementiert
    Es ist ab jetzt Möglich Config Dateien zu importieren, exportieren und
    es wird eine Default-Config geladen falls noch nicht vorhanden
    
    Issue: #10

--- MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IFileImporterImpl.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IFileImporterImpl.java
@@ -1,7 +1,14 @@
 package de.thkoeln.syp.mtc.steuerung.impl;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import de.thkoeln.syp.mtc.datenhaltung.impl.IConfigImpl;
 import de.thkoeln.syp.mtc.steuerung.services.IFileImporter;
@@ -10,9 +17,24 @@ public class IFileImporterImpl implements IFileImporter {
 
 	private IConfigImpl iConfigImpl;
 	private List<File> textdateien;
+	private Properties prop;
+
+	public IFileImporterImpl() {
+		iConfigImpl = null;
+		textdateien = new ArrayList<>();
+		prop = new Properties();
+
+		prop.setProperty(PROP_LEERZEICHEN, "false");
+		prop.setProperty(PROP_SATZZEICHEN, "false");
+		prop.setProperty(PROP_GROSSSCHREIBUNG, "false");
+		prop.setProperty(PROP_ROOT, System.getProperty("user.dir"));
+	}
 
 	@Override
 	public IConfigImpl getConfig() {
+		if (iConfigImpl == null)
+			importConfigdatei(DEFAULT_CONFIG);
+
 		return iConfigImpl;
 	}
 
@@ -22,9 +44,93 @@ public class IFileImporterImpl implements IFileImporter {
 	}
 
 	@Override
-	public boolean importConfigdatei() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean importConfigdatei(File config) {
+		iConfigImpl = new IConfigImpl();
+		OutputStream outputStream;
+		InputStream inputStream;
+
+		if (!config.exists()) {
+			if (!DEFAULT_CONFIG.exists()) {
+				try {
+					outputStream = new FileOutputStream(
+							System.getProperty("user.dir")
+									+ "/config.properties");
+
+					prop.store(outputStream, null);
+
+					iConfigImpl.setPath(System.getProperty("user.dir")
+							+ "/config.properties");
+
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			} else {
+				try {
+					inputStream = new FileInputStream(DEFAULT_CONFIG);
+
+					prop.load(inputStream);
+
+					iConfigImpl.setPath(DEFAULT_CONFIG.getAbsolutePath());
+
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+		} else {
+			try {
+				inputStream = new FileInputStream(config);
+
+				prop.load(inputStream);
+
+				iConfigImpl.setPath(config.getAbsolutePath());
+
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		iConfigImpl.setBeachteLeerzeichen(Boolean.parseBoolean(prop
+				.getProperty(PROP_LEERZEICHEN)));
+		iConfigImpl.setBeachteSatzzeichen(Boolean.parseBoolean(prop
+				.getProperty(PROP_SATZZEICHEN)));
+		iConfigImpl.setBeachteGrossschreibung(Boolean.parseBoolean(prop
+				.getProperty(PROP_GROSSSCHREIBUNG)));
+		iConfigImpl.setRootDir(prop.getProperty(PROP_ROOT));
+
+		return true;
+	}
+
+	@Override
+	public boolean exportConfigdatei(IConfigImpl iConfigImpl) {
+		Properties prop = new Properties();
+		OutputStream outputStream;
+
+		try {
+			outputStream = new FileOutputStream(iConfigImpl.getPath());
+
+			prop.setProperty(PROP_LEERZEICHEN,
+					Boolean.toString(iConfigImpl.getBeachteLeerzeichen()));
+			prop.setProperty(PROP_SATZZEICHEN,
+					Boolean.toString(iConfigImpl.getBeachteSatzzeichen()));
+			prop.setProperty(PROP_GROSSSCHREIBUNG,
+					Boolean.toString(iConfigImpl.getBeachteGrossschreibung()));
+			prop.setProperty(PROP_ROOT, iConfigImpl.getRootDir());
+
+			prop.store(outputStream, null);
+
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
--- MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/services/IFileImporter.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/services/IFileImporter.java
@@ -6,13 +6,22 @@ import java.util.List;
 import de.thkoeln.syp.mtc.datenhaltung.impl.IConfigImpl;
 
 public interface IFileImporter {
+	final String PROP_LEERZEICHEN = "beachteLeerzeichen";
+	final String PROP_SATZZEICHEN = "beachteSatzzeichen";
+	final String PROP_GROSSSCHREIBUNG = "beachteGrossschreibung";
+	final String PROP_ROOT = "rootDirectory";
+	final File DEFAULT_CONFIG = new File(System.getProperty("user.dir")
+			+ "/config.properties");
+
 	IConfigImpl getConfig();
 
 	List<File> getTextdateien();
-	
-	boolean importConfigdatei();
+
+	boolean importConfigdatei(File config);
+
+	boolean exportConfigdatei(IConfigImpl iConfigImpl);
 
 	boolean importTextdateien();
-	
+
 	boolean importTextRoot();
 }
