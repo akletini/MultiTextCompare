f41ea002355d703cf5def0d13355a6ed1121c96c
akletini <allen.kletinitch@smail.th-koeln.de>
2020-12-15 14:31:01 +0100

    Anpassungen an der GUI

--- MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/DateiauswahlView.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/DateiauswahlView.java
@@ -26,37 +26,35 @@ public class DateiauswahlView extends JFrame {
 	JFileChooser dateiSystem;
 	FileDialog fd;
 	private File[] auswahl;
-	
+
 	IFileImporter fileImport;
 	ITextvergleicher textVergleicher;
 	private IMatrixImpl matrix;
 
 	public DateiauswahlView() {
-		
+
 		panel = new JPanel();
 		dateiSystem = new JFileChooser();
 		fileImport = new IFileImporterImpl();
 		textVergleicher = new ITextvergleicherImpl();
 		matrix = new IMatrixImpl();
-		
-		
+
 		panel.setBorder(BorderFactory.createEmptyBorder(20, 60, 20, 60));
 		panel.setLayout(new GridLayout(0, 1));
-		
+
 		fd = new FileDialog(this, "Dateiauswahl", FileDialog.LOAD);
 		fd.setMultipleMode(true);
 		fd.setDirectory(".");
 		fd.setFile("*.txt");
 		fd.setVisible(true);
-		
+
 		int anzahlDateien = fd.getFiles().length;
 		String[] nameDateien = new String[anzahlDateien];
-		for(int i=0; i<anzahlDateien; i++){
+		for (int i = 0; i < anzahlDateien; i++) {
 			nameDateien[i] = fd.getFiles()[i].getName();
 		}
-		
-		
-		while(fd.getFiles().length == 1){
+
+		while (fd.getFiles().length == 1) {
 			new PopupView();
 			fd = new FileDialog(this, "Dateiauswahl", FileDialog.LOAD);
 			fd.setMultipleMode(true);
@@ -64,55 +62,53 @@ public class DateiauswahlView extends JFrame {
 			fd.setFile("*.txt");
 			fd.setVisible(true);
 		}
-		
-		
-//		dateiSystem.add(panel);
-//		Action details = dateiSystem.getActionMap().get("viewTypeDetails");
-//		details.actionPerformed(null);
-//		dateiSystem.setMultiSelectionEnabled(true);
-//		dateiSystem.setCurrentDirectory(new File("."));
-//		dateiSystem.setDialogTitle("Dateiauswahl");
-//		dateiSystem.setMultiSelectionEnabled(true);
-//		dateiSystem.showOpenDialog(panel);
-		
-		
-		
-		
-		//----------------------------------------------------------------
-		//Code der so in etwa in den Controller ausgelagert werden müsste
+
+		// dateiSystem.add(panel);
+		// Action details = dateiSystem.getActionMap().get("viewTypeDetails");
+		// details.actionPerformed(null);
+		// dateiSystem.setMultiSelectionEnabled(true);
+		// dateiSystem.setCurrentDirectory(new File("."));
+		// dateiSystem.setDialogTitle("Dateiauswahl");
+		// dateiSystem.setMultiSelectionEnabled(true);
+		// dateiSystem.showOpenDialog(panel);
+
+		// ----------------------------------------------------------------
+		// Code der so in etwa in den Controller ausgelagert werden müsste
 		auswahl = fd.getFiles();
 		fileImport.importTextdateien(fileArrayToList(getAuswahl()));
 		fileImport.createTempFiles();
 		textVergleicher.getTempfilesFromHashMap(fileImport.getTempFilesMap());
 		textVergleicher.getVergleiche(textVergleicher.getTempFiles());
-		textVergleicher.vergleicheUeberGanzesDokument();
+		if (fileImport.getConfig().getLineMatch() == false) {
+			textVergleicher.vergleicheUeberGanzesDokument();
+		} else {
+			textVergleicher.vergleicheZeilenweise();
+		}
 		matrix = textVergleicher.getMatrix();
-		MatrixView matrixView = new MatrixView(matrix, anzahlDateien, nameDateien);
-		//----------------------------------------------------------------
-		
-		
-		
+		new MatrixView(matrix, anzahlDateien,
+				nameDateien);
+		// ----------------------------------------------------------------
 
 		this.setLocationRelativeTo(null);
 	}
-	
-	public void actionPerformed(ActionEvent e){
-		
+
+	public void actionPerformed(ActionEvent e) {
+
 	}
-	
-	public File[] getAuswahl(){
+
+	public File[] getAuswahl() {
 		return auswahl;
 	}
-	
-	private List<File> fileArrayToList(File[] array){
+
+	private List<File> fileArrayToList(File[] array) {
 		List<File> fileListe = new ArrayList<>();
-		for(int i = 0; i < array.length; i++){
+		for (int i = 0; i < array.length; i++) {
 			fileListe.add(array[i]);
 		}
 		return fileListe;
 	}
-	
-	public IMatrix getMatrix(){
+
+	public IMatrix getMatrix() {
 		return matrix;
 	}
 
--- MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/MatrixView.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/MatrixView.java
@@ -25,57 +25,65 @@ public class MatrixView extends JFrame {
 	JPanel panel;
 	JTable table;
 	JButton testButton;
-	private static DecimalFormat df = new DecimalFormat("0.00");
+	private static DecimalFormat df = new DecimalFormat("0.000");
 	int index;
-	
-	public MatrixView(IMatrixImpl matrix, int anzahlDateien, String[] nameDateien){
+
+	public MatrixView(IMatrixImpl matrix, int anzahlDateien,
+			String[] nameDateien) {
 		panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(20, 60, 20, 60));
 		index = 0;
 		List<IAehnlichkeitImpl> list = matrix.getInhalt();
-		
+
 		String[][] data = new String[anzahlDateien][anzahlDateien];
 
-		for(int i=0; i<anzahlDateien; i++){
+		for (int i = 0; i < anzahlDateien; i++) {
 			data[i][i] = "1.0";
-			for(int j=i+1; j<anzahlDateien; j++){
+			for (int j = i + 1; j < anzahlDateien; j++) {
 				double wert = list.get(index).getWert();
 				String wertString = df.format(wert);
 				data[i][j] = wertString;
 				data[j][i] = wertString;
 				index++;
-				}
+			}
 		}
 		System.out.println(anzahlDateien);
 		table = new JTable(data, nameDateien);
-		panel.add(new JScrollPane(table));
-		
+		JScrollPane scrollPane = new JScrollPane(table);
+		RowNumberTable rowTable = new RowNumberTable(table);
+		rowTable.setFilenames(nameDateien);
+		scrollPane.setRowHeaderView(rowTable);
+		scrollPane.setCorner(JScrollPane.UPPER_LEFT_CORNER,
+				rowTable.getTableHeader());
+		panel.add(scrollPane);
+
 		this.add(panel, BorderLayout.CENTER);
 		this.setTitle("Matrix");
 		this.setSize(400, 400);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
 		this.pack();
-//		int r = (int) ((255* (100 - (wert*100))) / 100);
-//		int g = (int) ((255*(wert*100) / 100));
-//		Color  wertFarbe   = new Color(r, g,  0);
-		
-		//table.getColumnModel().getColumn(0).setCellRenderer(new RowHeaderRenderer());
-		
-//		for(int i=0; i<16; i++){
-//			double wert = Math.random();
-//			String wertString = df.format(wert);
-//			testButton = new JButton(""+ wertString);
-//			
-//			int r = (int) ((255* (100 - (wert*100))) / 100);
-//			int g = (int) ((255*(wert*100) / 100));
-//			Color  wertFarbe   = new Color(r, g,  0);
-//			
-//			testButton.setOpaque(true);
-//			testButton.setBackground(wertFarbe);
-//			testButton.setForeground(Color.BLACK);
-//		
-//			panel.add(testButton);
-//		}
+		// int r = (int) ((255* (100 - (wert*100))) / 100);
+		// int g = (int) ((255*(wert*100) / 100));
+		// Color wertFarbe = new Color(r, g, 0);
+
+		// table.getColumnModel().getColumn(0).setCellRenderer(new
+		// RowHeaderRenderer());
+
+		// for(int i=0; i<16; i++){
+		// double wert = Math.random();
+		// String wertString = df.format(wert);
+		// testButton = new JButton(""+ wertString);
+		//
+		// int r = (int) ((255* (100 - (wert*100))) / 100);
+		// int g = (int) ((255*(wert*100) / 100));
+		// Color wertFarbe = new Color(r, g, 0);
+		//
+		// testButton.setOpaque(true);
+		// testButton.setBackground(wertFarbe);
+		// testButton.setForeground(Color.BLACK);
+		//
+		// panel.add(testButton);
+		// }
 	}
 }
