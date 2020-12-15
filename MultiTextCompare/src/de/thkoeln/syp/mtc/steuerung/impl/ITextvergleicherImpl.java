61e397abf711d345742f544dec0587f0523264a3
akletini <allen.kletinitch@smail.th-koeln.de>
2020-12-15 12:44:00 +0100

    tempFile Liste reversed

--- MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/ITextvergleicherImpl.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/ITextvergleicherImpl.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -159,6 +160,7 @@ public class ITextvergleicherImpl implements ITextvergleicher {
 		for(Map.Entry<File, File> entry : map.entrySet()){
 			tempFiles.add(entry.getValue());
 		}
+		Collections.reverse(tempFiles);
 	}
 	
 	/**
