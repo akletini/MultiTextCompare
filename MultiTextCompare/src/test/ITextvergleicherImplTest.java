be83ad4a43f8c8a8539fe098935e9e7003947116
akletini <allen.kletinitch@smail.th-koeln.de>
2021-01-29 11:38:12 +0100

    Sysouts removed

--- MultiTextCompare/src/test/ITextvergleicherImplTest.java
+++ MultiTextCompare/src/test/ITextvergleicherImplTest.java
@@ -39,58 +39,30 @@ public class ITextvergleicherImplTest {
 
 	@Test
 	public void testVergleichsalorithmus() {
-		System.out.println("Vergleiche für Matrixaufbau");
 		paarungen = iText.getVergleiche(textdateien);
 		assertEquals(10, paarungen.size());
-
-		for (IAehnlichkeitImpl s : paarungen) {
-			System.out.println(s.getVon().getName()
-					.replaceFirst("[.][^.]+$", "")
-					+ "," + s.getZu().getName().replaceFirst("[.][^.]+$", ""));
-		}
-		System.out.println("");
 	}
 
 	@Test
 	public void test_vergleichWortFuerWort() {
-		System.out.println("Vergleiche WortFuerWort");
 		paarungen = iText.getVergleiche(textdateien);
 		double[] aehnlichkeit = new double[paarungen.size()];
 		iText.vergleicheZeilenweise();
 
 		for (int i = 0; i < paarungen.size(); i++) {
 			aehnlichkeit[i] = paarungen.get(i).getWert();
-			System.out.println("Von "
-					+ paarungen.get(i).getVon().getName()
-							.replaceFirst("[.][^.]+$", "")
-					+ " Zu "
-					+ paarungen.get(i).getZu().getName()
-							.replaceFirst("[.][^.]+$", "")
-					+ " mit Ähnlichkeit " + aehnlichkeit[i]);
 		}
-		System.out.println("");
 		assertEquals(0.5, aehnlichkeit[4], 0.0000001);
 	}
 
 	@Test
 	public void test_vergleichZeichenmenge() {
-		System.out.println("Vergleiche Zeichenmenge");
 		paarungen = iText.getVergleiche(textdateien);
 		double[] aehnlichkeit = new double[paarungen.size()];
-
 		iText.vergleicheUeberGanzesDokument();
-
 		for (int i = 0; i < paarungen.size(); i++) {
 			aehnlichkeit[i] = paarungen.get(i).getWert();
-			System.out.println("Von "
-					+ paarungen.get(i).getVon().getName()
-							.replaceFirst("[.][^.]+$", "")
-					+ " Zu "
-					+ paarungen.get(i).getZu().getName()
-							.replaceFirst("[.][^.]+$", "")
-					+ " mit Ähnlichkeit " + aehnlichkeit[i]);
 		}
-		System.out.println("");
 		assertEquals(0.5, aehnlichkeit[4], 0.0000001);
 	}
 
