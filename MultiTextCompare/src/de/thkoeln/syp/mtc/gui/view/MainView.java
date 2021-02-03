d6645418111b6385f6e450e66a849aa3a3eb7bd6
aschmi29 <alexander.schmitz@smail.th-koeln.de>
2021-02-03 18:27:38 +0100

    GUI: Bugfix Matrixfeld Auswahl

--- MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/MainView.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/gui/view/MainView.java
@@ -190,37 +190,41 @@ public class MainView extends JFrame {
 									.equals("temp_" + (columnIndex + 1))) {
 						selectedFiles.add(entry.getValue());
 						fileIndices.add(columnIndex);
-						management.getFileSelectionController()
-								.appendToTextArea(
-										management
-												.getFileSelectionView()
-												.getModel()
-												.get(fileIndices
-														.get(selectedFiles
-																.size() - 1))
-												.split(":")[0]
-												+ " has been selected. Total: "
-												+ (selectedFiles.size()));
+						fileIndices.add(rowIndex);
 
-						if (rowIndex == columnIndex) {
-							selectedFiles.add(entry.getValue());
-							management
-									.getFileSelectionController()
-									.appendToTextArea(
-											management
-													.getFileSelectionView()
-													.getModel()
-													.get(fileIndices.get(selectedFiles
-															.size() - 1))
-													.split(":")[0]
-													+ " has been selected. Total: "
-													+ (selectedFiles.size()));
-						}
+//						if (rowIndex == columnIndex) {
+//							selectedFiles.add(entry.getValue());
+//							management
+//									.getFileSelectionController()
+//									.appendToTextArea(
+//											management
+//													.getFileSelectionView()
+//													.getModel()
+//													.get(fileIndices.get(selectedFiles
+//															.size() - 1))
+//													.split(":")[0]
+//													+ " has been selected. Total: "
+//													+ (selectedFiles.size()));
+//						}
 					}
 				}
+				management.getFileSelectionController().appendToTextArea(
+						management.getFileSelectionView().getModel()
+								.get(fileIndices.get(selectedFiles.size() - 2))
+								.split(":")[0]
+								+ " has been selected. Total: "
+								+ (selectedFiles.size()-1));
+				
+				management.getFileSelectionController().appendToTextArea(
+						management.getFileSelectionView().getModel()
+								.get(fileIndices.get(selectedFiles.size() - 1))
+								.split(":")[0]
+								+ " has been selected. Total: "
+								+ (selectedFiles.size()));
+			}
 
-				// Klick auf Spaltenkopf
-			} else if (tableMatrix.getTableHeader().equals(e.getSource())) {
+			// Klick auf Spaltenkopf
+			else if (tableMatrix.getTableHeader().equals(e.getSource())) {
 				int columnIndex = tableMatrix.columnAtPoint(e.getPoint());
 				for (Map.Entry<File, File> entry : tempFiles) {
 					if (entry.getValue().getName()
