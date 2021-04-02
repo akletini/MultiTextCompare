b28b7dcdfdffdbfc50edf1173def65883bea9df8
akletini <allen.kletinitch@smail.th-koeln.de>
2021-04-02 13:09:57 +0200

    XML sort by value within sorted tag group

--- MultiTextCompare/src/de/thkoeln/syp/mtc/datenhaltung/api/IXMLElementComparator.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/datenhaltung/api/IXMLElementComparator.java
@@ -1,13 +1,19 @@
 package de.thkoeln.syp.mtc.datenhaltung.api;
 
 import java.util.Comparator;
+import java.util.List;
+
+import org.jdom2.Attribute;
 import org.jdom2.Element;
 
-public class IXMLElementComparator implements Comparator<Element>{
-	
-		@Override
-		public int compare(Element e1, Element e2) {
-			return e1.getName().compareTo(e2.getName());
+public class IXMLElementComparator implements Comparator<Element> {
+
+	@Override
+	public int compare(Element e1, Element e2) {
+		if (e1.getName().compareTo(e2.getName()) == 0) {
+			return e1.getValue().compareTo(e2.getValue());
 		}
-		
+		return e1.getName().compareTo(e2.getName());
+	}
+
 }
