fa500c7537c65a8af37d382886700f35dff51366
akletini <allen.kletinitch@smail.th-koeln.de>
2021-05-24 16:28:56 +0200

    Implement matching for xml lists

--- MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IXMLComparerImpl.java
+++ MultiTextCompare/src/de/thkoeln/syp/mtc/steuerung/impl/IXMLComparerImpl.java
@@ -12,8 +12,8 @@ import org.jdom2.Content;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
-import org.jdom2.ProcessingInstruction;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
 
 import de.thkoeln.syp.mtc.steuerung.services.ITextvergleicher;
 
@@ -52,14 +52,16 @@ public class IXMLComparerImpl {
 			Element currentRef = refFirstLevelChildren.get(i);
 			String currentRefName = currentRef.getName();
 			List<Element> compChildren = rootComp.getChildren(currentRefName);
-			
+
 			if (!compChildren.isEmpty()) {
-				Element currentComp = rootComp.getChildren(currentRefName).get(0);
-				compareContent(currentRef.getContent(), currentComp.getContent());
+				Element currentComp = rootComp.getChildren(currentRefName).get(
+						0);
+				compareContent(currentRef.getContent(),
+						currentComp.getContent());
 				// Liste von Knoten
 				if (hasChildren(currentRef) && hasChildren(currentComp)) {
-					similarity.add(compareElementsRecursively(currentRef, currentComp,
-							currentLevelWeight));
+					similarity.add(compareElementsRecursively(currentRef,
+							currentComp, currentLevelWeight));
 				}
 				// einzelnes Feld
 				else if (!hasChildren(currentRef) && !hasChildren(currentComp)) {
@@ -77,19 +79,20 @@ public class IXMLComparerImpl {
 		double contentWeight = 0.5;
 		double attributeWeight = 0.5;
 		double totalSimilarity = 0, contentSimilarity, attributeSimilarity;
-		double stringSimilarity = compareStrings(ref.getValue(), comp.getValue());
+		double stringSimilarity = compareStrings(ref.getValue(),
+				comp.getValue());
 		contentSimilarity = currentLevelWeight * contentWeight
 				* stringSimilarity;
 		attributeSimilarity = currentLevelWeight
 				* attributeWeight
 				* calcAttributeSimilarity(ref.getAttributes(),
 						comp.getAttributes());
-		if(hasAttributes(ref) || hasAttributes(comp)){
+		if (hasAttributes(ref) || hasAttributes(comp)) {
 			totalSimilarity = contentSimilarity + attributeSimilarity;
-		}else {
+		} else {
 			totalSimilarity = 2 * contentSimilarity;
 		}
-		
+
 		return totalSimilarity;
 	}
 
@@ -136,40 +139,69 @@ public class IXMLComparerImpl {
 
 		double currentWeight = calcLevelWeight(ref, comp);
 
-		int lastMatchedIndex = 0;
+		// get all elements with equal names which exist in both files
+		List<String> matchedElementNames = new ArrayList<String>();
 		for (int i = 0; i < ref.getChildren().size(); i++) {
 			Element currentRef = ref.getChildren().get(i);
-			for (int j = lastMatchedIndex; j < comp.getChildren().size(); j++) {
-				Element currentComp = comp.getChildren().get(j);
-				if (currentRef.getName().equals(currentComp.getName())) {
-					matchingRef.add(currentRef);
-					matchingComp.add(currentComp);
-					lastMatchedIndex = j + 1;
+			String currentRefName = currentRef.getName();
+			matchedElementNames.add(currentRefName);
+			if (getElementCount(matchedElementNames, currentRefName) == 1) {
+				matchingRef.addAll(copyElementList(ref
+						.getChildren(currentRefName)));
+				matchingComp.addAll(copyElementList(comp
+						.getChildren(currentRefName)));
+			}
+		}
+
+		// look for equal elements and remove them from the matched pool
+		for (int i = 0; i < matchingRef.size(); i++) {
+			Element currentRef = matchingRef.get(i);
+			for (int j = 0; j < matchingComp.size(); j++) {
+				Element currentComp = matchingComp.get(j);
+				if (currentComp == null) {
+					continue;
+				}
+				XMLOutputter xmlOut = new XMLOutputter();
+				String refString = xmlOut.outputString(currentRef);
+				String compString = xmlOut.outputString(currentComp);
+				boolean equals = refString.equals(compString);
+				if (equals) {
+					similarities.add(currentLevelWeight * currentWeight);
+					matchingRef.set(i, null);
+					matchingComp.set(j, null);
 					break;
 				}
 			}
 		}
+		matchingRef = clearNullValues(matchingRef);
+		matchingComp = clearNullValues(matchingComp);
 
-		for (int i = 0; i < matchingRef.size(); i++) {
+		int minSize = Math.min(matchingRef.size(), matchingComp.size());
+
+		// compare all leftover elements by index
+		for (int i = 0; i < minSize; i++) {
 			Element currentRef = matchingRef.get(i);
 			Element currentComp = matchingComp.get(i);
-			
+
 			List<Content> refContent = currentRef.getContent();
 			List<Content> compContent = currentComp.getContent();
-			
+
 			// BETA
 			compareContent(refContent, compContent);
-			
+
 			if (hasChildren(currentRef) && hasChildren(currentComp)) {
-				double similarity = currentLevelWeight * compareElementsRecursively(currentRef, currentComp,
-						currentWeight);
+				double similarity = currentLevelWeight
+						* compareElementsRecursively(currentRef, currentComp,
+								currentWeight);
 				similarities.add(similarity);
 			} else if (hasChildren(currentRef) && !hasChildren(currentComp)) {
 
 			} else if (hasChildren(currentRef) && !hasChildren(currentComp)) {
 
 			} else if (!hasChildren(currentRef) && !hasChildren(currentComp)) {
-				double similarity = currentLevelWeight * compareElements(currentRef, currentComp, currentWeight);
+				double similarity = currentLevelWeight
+						* compareElements(currentRef, currentComp,
+								currentWeight);
 				similarities.add(similarity);
 			}
 		}
@@ -184,39 +216,37 @@ public class IXMLComparerImpl {
 			List<Content> compContent) {
 		int maxSize = Math.max(refContent.size(), compContent.size());
 		int minSize = Math.min(refContent.size(), compContent.size());
-		
-		for(int i = 0; i < refContent.size(); i++){
+
+		for (int i = 0; i < refContent.size(); i++) {
 			Object ref = refContent.get(i);
-			for(int j = 0; j < compContent.size(); j++){
+			for (int j = 0; j < compContent.size(); j++) {
 				Object comp = compContent.get(j);
-				if(ref instanceof Comment && comp instanceof Comment){
+				if (ref instanceof Comment && comp instanceof Comment) {
 					compareComments((Comment) ref, (Comment) comp);
 					break;
-				}
-				else if(ref instanceof CDATA && comp instanceof CDATA){
+				} else if (ref instanceof CDATA && comp instanceof CDATA) {
 					compareCDATA((CDATA) ref, (CDATA) comp);
 					break;
 				}
 			}
 		}
-		
+
 	}
-	
-	private double compareComments(Comment ref, Comment comp){
+
+	private double compareComments(Comment ref, Comment comp) {
 		return compareStrings(ref.getText(), comp.getText());
 	}
-	
-	private double compareCDATA(CDATA ref, CDATA comp){
+
+	private double compareCDATA(CDATA ref, CDATA comp) {
 		return compareStrings(ref.getTextNormalize(), comp.getTextNormalize());
 	}
-	
 
 	private double compareStrings(String ref, String comp) {
 		double similarity = 1.0;
 		double maxLength = Math.max(ref.length(), comp.length());
-		if(maxLength > 0.0){
-		similarity = 1 - (textvergleicher.calculateLevenshteinDist(ref,
-				comp, maxLineLength) / maxLength);
+		if (maxLength > 0.0) {
+			similarity = 1 - (textvergleicher.calculateLevenshteinDist(ref,
+					comp, maxLineLength) / maxLength);
 		}
 		return similarity;
 	}
@@ -236,13 +266,14 @@ public class IXMLComparerImpl {
 		}
 		return true;
 	}
-	
-	private boolean hasAttributes(Element e){
-		if(e.getAttributes().size() == 0){
+
+	private boolean hasAttributes(Element e) {
+		if (e.getAttributes().size() == 0) {
 			return false;
 		}
 		return true;
 	}
+
 	private double sumSimilarities(List<Double> similarities) {
 		double similarity = 0.0;
 		for (Double d : similarities) {
@@ -250,8 +281,36 @@ public class IXMLComparerImpl {
 		}
 		return similarity;
 	}
-	
-	public List<Double> getSimilarities(){
+
+	private List<Element> clearNullValues(List<Element> original) {
+		List<Element> returnList = new ArrayList<Element>();
+		for (Element e : original) {
+			if (e != null) {
+				returnList.add(e);
+			}
+		}
+		return returnList;
+	}
+
+	private List<Element> copyElementList(List<Element> original) {
+		List<Element> returnList = new ArrayList<Element>();
+		for (Element e : original) {
+			returnList.add(e);
+		}
+		return returnList;
+	}
+
+	private int getElementCount(List<String> original, String name) {
+		int count = 0;
+		for (String s : original) {
+			if (s.equals(name)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public List<Double> getSimilarities() {
 		return similarities;
 	}
 }
