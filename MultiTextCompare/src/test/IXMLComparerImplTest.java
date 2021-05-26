f19fdd645ef8e31b3141b43cc51084f981053257
akletini <allen.kletinitch@smail.th-koeln.de>
2021-05-27 01:01:49 +0200

    Adjust xml compare tests for matching functionality

--- MultiTextCompare/src/test/IXMLComparerImplTest.java
+++ MultiTextCompare/src/test/IXMLComparerImplTest.java
@@ -15,7 +15,7 @@ import de.thkoeln.syp.mtc.steuerung.impl.IXMLComparerImpl;
 public class IXMLComparerImplTest {
 	
 	static IXMLComparerImpl xmlCompare;
-	static File attrTest1, attrTest2;
+	static File attrTest1, attrTest2,attrTest3, attrTest4, attrTest5, attrTest6,attrTest7, attrTest8;
 	static File recTest1, recTest2;
 	
 	@BeforeClass
@@ -25,6 +25,19 @@ public class IXMLComparerImplTest {
 				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes1.xml");
 		attrTest2 = new File(System.getProperty("user.dir")
 				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes2.xml");
+		attrTest3 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes3.xml");
+		attrTest4 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes4.xml");
+		attrTest5 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes5.xml");
+		attrTest6 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes6.xml");
+		attrTest7 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes7.xml");
+		attrTest8 = new File(System.getProperty("user.dir")
+				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestAttributes8.xml");
+		
 		recTest1 = new File(System.getProperty("user.dir")
 				+ File.separator + "/src/test/testFiles/IXMLCompareTestFiles/TestRecursionList1.xml");
 		recTest2 = new File(System.getProperty("user.dir")
@@ -43,32 +56,39 @@ public class IXMLComparerImplTest {
 	
 	@Test
 	public void test_differentElementValuesBothHaveAttributes() throws IOException, JDOMException{
-		xmlCompare.compare(attrTest1, attrTest2);
+		xmlCompare.compare(attrTest7, attrTest8);
 		List<Double> similarities = xmlCompare.getSimilarities();
-		assertEquals(0.75 * 1/8 ,similarities.get(1).doubleValue(), 0.0001);
+		assertEquals(0.75,similarities.get(0).doubleValue(), 0.0001);
+		xmlCompare.compare(attrTest8, attrTest7);
+		similarities = xmlCompare.getSimilarities();
+		assertEquals(0.75,similarities.get(0).doubleValue(), 0.0001);
 	}
 	
 	@Test
 	public void test_equalElementValuesOneWithoutAttributesIsSymmetrical() throws IOException, JDOMException{
-		xmlCompare.compare(attrTest1, attrTest2);
+		xmlCompare.compare(attrTest3, attrTest4);
 		List<Double> similarities = xmlCompare.getSimilarities();
-		assertEquals(0.5 * 1/8 ,similarities.get(2).doubleValue(), 0.0001);
-		assertEquals(0.5 * 1/8 ,similarities.get(3).doubleValue(), 0.0001);
+		assertEquals(0.5 ,similarities.get(0).doubleValue(), 0.0001);
+		xmlCompare.compare(attrTest4, attrTest3);
+		similarities = xmlCompare.getSimilarities();
+		assertEquals(0.5 ,similarities.get(0).doubleValue(), 0.0001);
 	}
 	
 	@Test
 	public void test_differentElementValuesOneWithoutAttributesIsSymmetrical() throws IOException, JDOMException{
-		xmlCompare.compare(attrTest1, attrTest2);
+		xmlCompare.compare(attrTest5, attrTest6);
 		List<Double> similarities = xmlCompare.getSimilarities();
-		assertEquals(0.25 * 1/8 ,similarities.get(4).doubleValue(), 0.0001);
-		assertEquals(0.25 * 1/8 ,similarities.get(5).doubleValue(), 0.0001);
+		assertEquals(0.25 ,similarities.get(0).doubleValue(), 0.0001);
+		xmlCompare.compare(attrTest6, attrTest5);
+		similarities = xmlCompare.getSimilarities();
+		assertEquals(0.25 ,similarities.get(0).doubleValue(), 0.0001);
 	}
 	
 	@Test
 	public void test_equalElementValuesWithoutAttributes() throws IOException, JDOMException{
 		xmlCompare.compare(attrTest1, attrTest2);
 		List<Double> similarities = xmlCompare.getSimilarities();
-		assertEquals(1.0 * 1/8 ,similarities.get(6).doubleValue(), 0.0001);
+		assertEquals(1.0 * 1.0/8.0 ,similarities.get(2).doubleValue(), 0.0001);
 	}
 	@Test
 	public void test_differentElementValuesWithoutAttributes() throws IOException, JDOMException{
